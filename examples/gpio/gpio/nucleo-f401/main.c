0e72667da904a7e5ba8a0f1629b953d91895f094
uqembeddedsys <50263708+uqembeddedsys@users.noreply.github.com>
2022-02-18 02:35:55 +1000

    updating gpio examples
    updating gpio examples

---
+++ examples/gpio/gpio/nucleo-f401/main.c
@@ -0,0 +1,90 @@
+/**
+  ******************************************************************************
+  * @file    gpio/main.c
+  * @author  MDS
+  * @date    27112017
+  * @brief   Nucleo onboard GPIO pin read and write, using Pins B3 (Board D3)
+  *          B5 (Board D4).
+  *          To view the output, run the debugger to view the GPIOB ODR and IDR.
+  ******************************************************************************
+  */
+
+#include "board.h"
+#include "processor_hal.h"
+
+void Hardware_init(void);
+
+/**
+  * @brief  Main program - flashes on board LEDs
+  * @param  None
+  * @retval None
+  */
+int main(void)  {
+
+	uint16_t write_value = 0;
+
+	HAL_Init();			//Initialise board.
+
+	Hardware_init();	//Initialise hardware modules
+
+	// Main processing loop
+  while (1) {
+
+    // Toggle pin A3 high or low
+    write_value = ~write_value;
+
+    if ((write_value & 0x01) == 0x01){
+
+      GPIOB->ODR |= (0x01 << 3);   //Set the bit in the ODR
+    } else {  
+
+      GPIOB->ODR &= ~(0x01 << 3);   //Clear the bit in the ODR
+    }
+
+    //Turn Blue LED on, if pin A2 is high
+    if ((GPIOB->IDR & (0x0001 << 5)) == (0x0001 << 5)){
+      
+      BRD_LEDGreenOn(); 
+    } else {  
+
+      BRD_LEDGreenOff();
+    } 
+  }
+
+  return 0;
+}
+
+/*
+ * Initialise Hardware
+ */
+void Hardware_init(void) {
+
+  BRD_LEDInit();		//Initialise LEDS
+	BRD_LEDGreenOff();
+
+	// Enable the GPIO A Clock
+  __GPIOB_CLK_ENABLE();
+
+  //Initialise A3 as an output.
+  GPIOB->MODER &= ~(0x03 << (3 *2));  //clear bits
+  GPIOB->MODER |= (0x01 << (3 *2));   //Set for push pull
+
+  GPIOB->OSPEEDR &= ~(0x03<<(3 *2));
+	GPIOB->OSPEEDR |=   0x02<<(3 *2);  // Set for Fast speed
+
+  GPIOB->OTYPER &= ~(0x01 << 3);       //Clear Bit for Push/Pull output
+
+  // Activate the Pull-up or Pull down resistor for the current IO
+  GPIOB->PUPDR &= ~(0x03 << (3 *2));   //Clear Bits
+  GPIOB->PUPDR |= ((0x01) << (3 *2));  //Set for Pull down output
+  
+  // Initalise A2 as an input.
+  GPIOB->MODER &= ~(0x03 << (5 * 2));			//Clear bits for input mode
+
+  GPIOB->OSPEEDR &= ~(0x03<<(5 * 2));
+	GPIOB->OSPEEDR |=   0x02<<(5 * 2);  // Fast speed
+
+	GPIOB->PUPDR &= ~(0x03 << (5 * 2));			//Clear bits for no push/pull
+  GPIOB->PUPDR |= ((0x02) << (5 * 2));  //Set for Pull down output
+
+}
