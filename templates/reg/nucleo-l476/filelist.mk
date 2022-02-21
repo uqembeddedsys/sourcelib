82388e154d88d25058b538491fa1f3032b848c8f
uqembeddedsys <50263708+uqembeddedsys@users.noreply.github.com>
2022-02-21 22:06:39 +1000

    updating repo folder path in filelist.mk

--- templates/reg/nucleo-l476/filelist.mk
+++ templates/reg/nucleo-l476/filelist.mk
@@ -7,7 +7,7 @@
 # ONLY use relative file paths that start with $(SOURCELIB_ROOT)../
 # DO NOT USE absolute file paths (e.g. /home/users/myuser/mydir)
 # e.g. MYLIBPATH=$(SOURCELIB_ROOT)/../mylib
-MYLIB_PATH=$(SOURCELIB_ROOT)/../mylib
+MYLIB_PATH=$(SOURCELIB_ROOT)/../repo/mylib
 
 # Set folder path with header files to include.
 # ONLY use relative file paths that start with $(SOURCELIB_ROOT)../
