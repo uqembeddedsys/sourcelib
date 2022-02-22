b4ab98689fe3675ea393af9b7505f33ae9184377
uqembeddedsys <50263708+uqembeddedsys@users.noreply.github.com>
2022-02-22 19:31:56 +1000

    Update sourcelib_install.sh

--- tools/sourcelib_install.sh
+++ tools/sourcelib_install.sh
@@ -8,8 +8,8 @@ sudo apt-get remove modemmanager brltty	#Remove serial terminal interference
 sudo adduser $USER dialout		#Add user permissions for dialout
 sudo apt-get install -y python3-pip
 mkdir $HOME/Downloads
-wget -O $HOME/Downloads/JLink_Linux_V696_x86_64.deb https://cloudstor.aarnet.edu.au/plus/s/1nCIZgLudE9s9Dw/download
-sudo dpkg -i $HOME/Downloads/JLink_Linux_V696_x86_64.deb
+wget -O $HOME/Downloads/JLink_Linux_V762_x86_64.deb https://cloudstor.aarnet.edu.au/plus/s/alRMOQKscp2JX74
+sudo dpkg -i $HOME/Downloads/JLink_Linux_V762_x86_64.deb
 sudo python3 -m pip install pylink-square
 sudo ln -s /usr/bin/gdb-multiarch /usr/local/bin/arm-none-eabi-gdb
 /bin/bash $PWD/sourcelib_env.sh
