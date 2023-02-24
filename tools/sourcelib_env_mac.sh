9733cd9c1bc19caf481ff6fd5a217ec3ff6b9207
uqembeddedsys <50263708+uqembeddedsys@users.noreply.github.com>
2023-02-24 15:04:22 +1000

    Update sourcelib_env_mac.sh

--- tools/sourcelib_env_mac.sh
+++ tools/sourcelib_env_mac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
+#DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 
 # add sourcelib and path to profile
 echo export 'SOURCELIB_ROOT=$HOME/csse3010/sourcelib' >> ~/.zprofile
@@ -8,4 +8,4 @@ echo export 'PATH=$HOME/.local/bin:$PATH' >> ~/.zprofile
 echo export 'PATH=/opt/SEGGER/JLink:$PATH' >> ~/.zprofile
 
 # update environment variables
-source ~/.profile
+#source ~/.profile
