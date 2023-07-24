a176189aac0eb26894732ceed0c1a71f8533017d
uqembeddedsys <50263708+uqembeddedsys@users.noreply.github.com>
2023-07-24 12:02:54 +1000

    updating wsl installation scripts

--- tools/sourcelib_wsl.sh
+++ tools/sourcelib_wsl.sh
@@ -9,9 +9,9 @@ sudo apt-get install -y libnewlib-arm-none-eabi	#Install ARM new library
 sudo adduser $USER dialout		#Add user permissions for dialout
 sudo apt --fix-broken install -y
 sudo apt-get install -y python3-pip
-mkdir /mnt/c/CSSE3010/Downloads
-wget -O /mnt/c/CSSE3010/Downloads/JLink_Linux_V762_x86_64.deb https://cloudstor.aarnet.edu.au/plus/s/alRMOQKscp2JX74/download
-sudo dpkg -i /mnt/c/CSSE3010/Downloads/JLink_Linux_V762_x86_64.deb
+mkdir /mnt/c/csse3010/Downloads
+wget -O /mnt/c/csse3010/Downloads/JLink_Linux_V762_x86_64.deb https://cloudstor.aarnet.edu.au/plus/s/alRMOQKscp2JX74/download
+sudo dpkg -i /mnt/c/csse3010/Downloads/JLink_Linux_V762_x86_64.deb
 sudo python3 -m pip install pylink-square
 sudo ln -s /usr/bin/gdb-multiarch /usr/local/bin/arm-none-eabi-gdb
 /bin/bash $PWD/sourcelib_env_linux.sh
