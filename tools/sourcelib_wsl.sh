de9ba86d693ca1b83377672a4804231840c446d8
MattDsouza <mattdsouza80@gmail.com>
2024-02-15 01:38:16 +1000

    Update sourcelib_wsl.sh

--- tools/sourcelib_wsl.sh
+++ tools/sourcelib_wsl.sh
@@ -10,9 +10,10 @@ sudo adduser $USER dialout		#Add user permissions for dialout
 sudo apt-get install -y libxkbcommon-x11-0
 sudo apt --fix-broken install -y
 sudo apt-get install -y python3-pip
-mkdir /mnt/c/csse3010/Downloads
-wget -O /mnt/c/csse3010/Downloads/JLink_Linux_V762_x86_64.deb https://cloudstor.aarnet.edu.au/plus/s/alRMOQKscp2JX74/download
-sudo dpkg -i /mnt/c/csse3010/Downloads/JLink_Linux_V762_x86_64.deb
+mkdir $HOME/csse3010
+mkdir $HOME/csse3010/Downloads
+wget -O $HOME/csse3010/Downloads/JLink_Linux_V794j_x86_64.deb https://github.com/uqembeddedsys/sourcelib/releases/download/jlink/JLink_Linux_V794j_x86_64.deb
+sudo dpkg -i $HOME/csse3010/Downloads/JLink_Linux_V794j_x86_64.deb
 sudo python3 -m pip install pylink-square
 sudo ln -s /usr/bin/gdb-multiarch /usr/local/bin/arm-none-eabi-gdb
 /bin/bash $PWD/sourcelib_env_wsl.sh
