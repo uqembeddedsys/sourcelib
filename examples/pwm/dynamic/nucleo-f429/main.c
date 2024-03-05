a33af1d1aa0cd790b8efd33a2fb2c680a4f085a9
uqembeddedsys <50263708+uqembeddedsys@users.noreply.github.com>
2024-03-06 01:11:16 +1000

    updated PWM examples for MFS

--- examples/pwm/dynamic/nucleo-f429/main.c
+++ examples/pwm/dynamic/nucleo-f429/main.c
@@ -3,8 +3,9 @@
   * @file    pwm/dynamic/
   * @author  MDS
   * @date    02032018
-  * @brief   Enable a dynamic (varying) PWM output on Board Pin D22 (PB5).
-  *			 See Section 18 (TIM3), P592 of the STM32F4xx Reference Manual.
+  * @brief   Enable a static (not changing) PWM output on Board Pin D5 (PE11).
+  *			 See Section 18 (TIM1), P592 of the STM32F4xx Reference Manual.
+  *			 Multi-Functional Shield (MFS) Pin PWM signal (~5)
   ******************************************************************************
   *
   */
@@ -15,9 +16,9 @@
 
 #define TIMER_COUNTER_FREQ  				5000           				//Frequency (in Hz)
 #define PWM_PULSE_WIDTH_SECONDS				2						//2s
-#define PWM_PULSE_WIDTH_TICKS				10000 					//((PWM_PULSE_WIDTH_SECONDS)/(1/TIMER_COUNTER_FREQ))		//Period in timer ticks
+#define PWM_PULSE_WIDTH_TICKS				20000 					//2*((PWM_PULSE_WIDTH_SECONDS)/(1/TIMER_COUNTER_FREQ))		//Period in timer ticks
 #define PWM_PERCENT2TICKS_DUTYCYCLE(value)	(uint16_t)(value*PWM_PULSE_WIDTH_TICKS/100)				//Duty Cycle on time in timer ticks
-#define PWM_DUTYCYCLE_CHANGE(value) 			TIM3->CCR2=(uint16_t)value									//Change PWM dutycyle
+#define PWM_DUTYCYCLE_CHANGE(value) 			TIM1->CCR2=(uint16_t)value									//Change PWM dutycyle
 
 void hardware_init(void);
 
@@ -50,48 +51,48 @@ int main(void) {
  */
 void hardware_init(void) {
 
-	BRD_LEDInit();		//Initialise LEDS
+	BRD_LEDInit();		//Initialise LEDs
 
 	// Turn off LEDs
 	BRD_LEDGreenOff();
 
-	// Enable GPIOC Clock
-	__GPIOB_CLK_ENABLE();
+	// Enable GPIOE Clock
+	__GPIOE_CLK_ENABLE();
 
-    GPIOB->OSPEEDR |= (GPIO_SPEED_FAST << (5 * 2));		//Set fast speed.
-	GPIOB->PUPDR &= ~(0x03 << (5 * 2));				//Clear bits for no push/pull
-	GPIOB->MODER &= ~(0x03 << (5 * 2));				//Clear bits
-	GPIOB->MODER |= (GPIO_MODE_AF_PP << (5 * 2));  	//Set Alternate Function Push Pull Mode
+    GPIOE->OSPEEDR |= (GPIO_SPEED_FAST << (11 * 2));		//Set fast speed.
+	GPIOE->PUPDR &= ~(0x03 << (11 * 2));				//Clear bits for no push/pull
+	GPIOE->MODER &= ~(0x03 << (11 * 2));				//Clear bits
+	GPIOE->MODER |= (GPIO_MODE_AF_PP << (11 * 2));  	//Set Alternate Function Push Pull Mode
 	
-	GPIOB->AFR[0] &= ~((0x0F) << (5 * 4));			//Clear Alternate Function for pin (lower ARF register)
-	GPIOB->AFR[0] |= (GPIO_AF2_TIM3 << (5 * 4));	//Set Alternate Function for pin (lower ARF register) 
+	GPIOE->AFR[1] &= ~((0x0F) << ((11-8) * 4));			//Clear Alternate Function for pin (lower ARF register)
+	GPIOE->AFR[1] |= (GPIO_AF1_TIM1 << ((11-8) * 4));	//Set Alternate Function for pin (lower ARF register) 
 
 	// Timer 3 clock enable
-	__TIM3_CLK_ENABLE();
+	__TIM1_CLK_ENABLE();
 
 	/* Compute the prescaler value
 	   Set the clock prescaler to 50kHz
 	   SystemCoreClock is the system clock frequency */
-	TIM3->PSC = ((SystemCoreClock / 2) / TIMER_COUNTER_FREQ) - 1;
+	TIM1->PSC = ((SystemCoreClock / 2) / TIMER_COUNTER_FREQ) - 1;
 
 	// Counting direction: 0 = up-counting, 1 = down-counting (Timer Control Register 1)
-	TIM3->CR1 &= ~TIM_CR1_DIR; 
+	TIM1->CR1 &= ~TIM_CR1_DIR; 
 
-	TIM3->ARR = PWM_PULSE_WIDTH_TICKS; 		//Set pulse period to ~1s 
-	TIM3->CCR2 = PWM_PERCENT2TICKS_DUTYCYCLE(50);	//Set pulse width to 0% duty cycle.
+	TIM1->ARR = PWM_PULSE_WIDTH_TICKS; 		//Set pulse period to ~1s 
+	TIM1->CCR2 = PWM_PERCENT2TICKS_DUTYCYCLE(50);	//Set pulse width to 0% duty cycle.
 
-	TIM3->CCMR1 &= ~(TIM_CCMR1_OC2M); 	// Clear OC2M (Channel 2) 
-	TIM3->CCMR1 |= (0x6 << 12); 		// Enable PWM Mode 1, upcounting, on Channel 2 
-	TIM3->CCMR1 |= (TIM_CCMR1_OC2PE); 	// Enable output preload bit for channel 2 
+	TIM1->CCMR1 &= ~(TIM_CCMR1_OC2M); 	// Clear OC2M (Channel 2) 
+	TIM1->CCMR1 |= (0x6 << 12); 		// Enable PWM Mode 1, upcounting, on Channel 2 
+	TIM1->CCMR1 |= (TIM_CCMR1_OC2PE); 	// Enable output preload bit for channel 2 
 	
-	TIM3->CR1  |= (TIM_CR1_ARPE); 	// Set Auto-Reload Preload Enable 
-	TIM3->CCER |= TIM_CCER_CC2E; 	// Set CC2E Bit
-	TIM3->CCER &= ~TIM_CCER_CC2NE; 	// Clear CC2NE Bit for active high output
+	TIM1->CR1  |= (TIM_CR1_ARPE); 	// Set Auto-Reload Preload Enable 
+	TIM1->CCER |= TIM_CCER_CC2E; 	// Set CC2E Bit
+	TIM1->CCER &= ~TIM_CCER_CC2NE; 	// Clear CC2NE Bit for active high output
 	
 	/* Set Main Output Enable (MOE) bit
 	   Set Off-State Selection for Run mode (OSSR) bit
 	   Set Off-State Selection for Idle mode (OSSI) bit */
-	TIM3->BDTR |= TIM_BDTR_MOE | TIM_BDTR_OSSR | TIM_BDTR_OSSI; 
+	TIM1->BDTR |= TIM_BDTR_MOE | TIM_BDTR_OSSR | TIM_BDTR_OSSI; 
 
-	TIM3->CR1 |= TIM_CR1_CEN;	// Enable the counter
+	TIM1->CR1 |= TIM_CR1_CEN;	// Enable the counter
 }
--- examples/pwm/static/nucleo-f429/main.c
+++ examples/pwm/static/nucleo-f429/main.c
@@ -2,9 +2,10 @@
   ******************************************************************************
   * @file    pwm/staic/main.c
   * @author  MDS
-  * @date    02032018
-  * @brief   Enable a static (not changing) PWM output on Board Pin D22 (PB5).
+  * @date    03032024
+  * @brief   Enable a static (not changing) PWM output on Board Pin D5 (PE11).
   *			 See Section 18 (TIM3), P592 of the STM32F4xx Reference Manual.
+  *			 Multi-Functional Shield (MFS) Pin PWM signal (~5)
   ******************************************************************************
   *
   */
@@ -13,8 +14,9 @@
 #include "board.h"
 #include "processor_hal.h"
 
-#define TIMER_COUNTER_FREQ  		50000           							//Frequency (in Hz)
-#define TIMER_1SECOND_PERIOD_TICKS		50000 									//1/(1/TIMER_COUNTER_FREQ) is the Period in timer ticks
+#define TIMER_COUNTER_FREQ  		25000           							//Frequency (in Hz)
+#define TIMER_1SECOND_PERIOD_TICKS		TIMER_COUNTER_FREQ*2 					//2*(1/(1/TIMER_COUNTER_FREQ)) is the Period in timer ticks. 
+																				//Note a period consists of two zero crossings.
 #define TIMER_50PERCENT_DUTYCYLE_TICKS	50*TIMER_1SECOND_PERIOD_TICKS/100		//Duty Cycle on time in timer ticks
 
 void hardware_init(void);
@@ -46,42 +48,42 @@ void hardware_init(void) {
 	BRD_LEDGreenOff();
 
 	// Enable GPIO Clock
-	__GPIOB_CLK_ENABLE();
+	__GPIOE_CLK_ENABLE();
 
-    GPIOB->OSPEEDR |= (GPIO_SPEED_FAST << (5 * 2));		//Set fast speed.
-	GPIOB->PUPDR &= ~(0x03 << (5 * 2));				//Clear bits for no push/pull
-	GPIOB->MODER &= ~(0x03 << (5 * 2));				//Clear bits
-	GPIOB->MODER |= (GPIO_MODE_AF_PP << (5 * 2));  	//Set Alternate Function Push Pull Mode
+    GPIOE->OSPEEDR |= (GPIO_SPEED_FAST << (11 * 2));		//Set fast speed.
+	GPIOE->PUPDR &= ~(0x03 << (11 * 2));				//Clear bits for no push/pull
+	GPIOE->MODER &= ~(0x03 << (11 * 2));				//Clear bits
+	GPIOE->MODER |= (GPIO_MODE_AF_PP << (11 * 2));  	//Set Alternate Function Push Pull Mode
 	
-	GPIOB->AFR[0] &= ~((0x0F) << (5 * 4));			//Clear Alternate Function for pin (lower ARF register)
-	GPIOB->AFR[0] |= (GPIO_AF2_TIM3 << (5 * 4));	//Set Alternate Function for pin (lower ARF register) 
+	GPIOE->AFR[1] &= ~((0x0F) << ((11-8) * 4));			//Clear Alternate Function for pin (lower ARF register)
+	GPIOE->AFR[1] |= (GPIO_AF1_TIM1 << ((11-8) * 4));	//Set Alternate Function for pin (lower ARF register) 
 
-	// Timer 3 clock enable
-	__TIM3_CLK_ENABLE();
+	// Timer 1 clock enable
+	__TIM1_CLK_ENABLE();
 
 	/* Compute the prescaler value
 	   Set the clock prescaler to 50kHz
 	   SystemCoreClock is the system clock frequency */
-	TIM3->PSC = ((SystemCoreClock / 2) / TIMER_COUNTER_FREQ) - 1;
+	TIM1->PSC = ((SystemCoreClock / 2) / TIMER_COUNTER_FREQ) - 1;
 
 	// Counting direction: 0 = up-counting, 1 = down-counting (Timer Control Register 1)
-	TIM3->CR1 &= ~TIM_CR1_DIR; 
+	TIM1->CR1 &= ~TIM_CR1_DIR; 
 
-	TIM3->ARR = TIMER_1SECOND_PERIOD_TICKS; 		//Set pulse period to ~1s 
-	TIM3->CCR2 = TIMER_50PERCENT_DUTYCYLE_TICKS;	//Set pulse width to 50% duty cycle.
+	TIM1->ARR = TIMER_1SECOND_PERIOD_TICKS; 		//Set pulse period to ~1s 
+	TIM1->CCR2 = TIMER_50PERCENT_DUTYCYLE_TICKS;	//Set pulse width to 50% duty cycle.
 
-	TIM3->CCMR1 &= ~(TIM_CCMR1_OC2M); 	// Clear OC2M (Channel 2) 
-	TIM3->CCMR1 |= (0x6 << 12); 		// Enable PWM Mode 1, upcounting, on Channel 2 
-	TIM3->CCMR1 |= (TIM_CCMR1_OC2PE); 	// Enable output preload bit for channel 2 
+	TIM1->CCMR1 &= ~(TIM_CCMR1_OC2M); 	// Clear OC2M (Channel 2) 
+	TIM1->CCMR1 |= (0x6 << 12); 		// Enable PWM Mode 1, upcounting, on Channel 2 
+	TIM1->CCMR1 |= (TIM_CCMR1_OC2PE); 	// Enable output preload bit for channel 2 
 	
-	TIM3->CR1  |= (TIM_CR1_ARPE); 	// Set Auto-Reload Preload Enable 
-	TIM3->CCER |= TIM_CCER_CC2E; 	// Set CC2E Bit 
-	TIM3->CCER &= ~TIM_CCER_CC2NE; 	// Clear CC2NE Bit for active high output 
+	TIM1->CR1  |= (TIM_CR1_ARPE); 	// Set Auto-Reload Preload Enable 
+	TIM1->CCER |= TIM_CCER_CC2E; 	// Set CC2E Bit 
+	TIM1->CCER &= ~TIM_CCER_CC2NE; 	// Clear CC2NE Bit for active high output 
 	
 	/* Set Main Output Enable (MOE) bit
 	   Set Off-State Selection for Run mode (OSSR) bit
 	   Set Off-State Selection for Idle mode (OSSI) bit */
-	TIM3->BDTR |= TIM_BDTR_MOE | TIM_BDTR_OSSR | TIM_BDTR_OSSI; 
+	TIM1->BDTR |= TIM_BDTR_MOE | TIM_BDTR_OSSR | TIM_BDTR_OSSI; 
 
-	TIM3->CR1 |= TIM_CR1_CEN;	// Enable the counter
+	TIM1->CR1 |= TIM_CR1_CEN;	// Enable the counter
 }
