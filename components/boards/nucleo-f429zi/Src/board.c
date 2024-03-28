0b44d1935065ae3459dfbd808a8a3710ac00c004
uqembeddedsys <50263708+uqembeddedsys@users.noreply.github.com>
2024-03-28 16:12:04 +1000

    Update board.c

--- components/boards/nucleo-f429zi/Src/board.c
+++ components/boards/nucleo-f429zi/Src/board.c
@@ -204,11 +204,15 @@ unsigned char BRD_debuguart_getc(int blocktime) {
 	uint8_t rx_char = '\0';
 
 	//Non Block receive - 0 delay (set to HAL_MAX_DELAY for blocking)
+	if (blocktime == 0) { 
+		rx_char = UART_debug.Instance->DR;
+	} else {
 	if (HAL_UART_Receive(&UART_debug, &rx_char, 1, blocktime) == HAL_OK) {
 		return rx_char;
 	} else {
 		return '\0';
 	}
+}
 
 	//return (uint8_t)(__HAL_UART_FLUSH_DRREGISTER(&UART_debug) & (uint8_t)0x00FF);
 }
